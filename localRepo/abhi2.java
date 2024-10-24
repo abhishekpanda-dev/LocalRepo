b88865572149f9b95c0e9aa13e3f08c0bc5e4f4a
AbhishekPanda <abhishekpanda.main@gmail.com>
2024-10-24 11:46:42 +0530

    Add new feature

--- localRepo/abhi2.java
+++ localRepo/abhi2.java
@@ -4,5 +4,6 @@ public class abhi2 {
     public static void main(String args[]){
         System.out.println("nme");
         System.out.println("my name is pandaS");
+        System.out.println("Add new feature");
     }
 }
